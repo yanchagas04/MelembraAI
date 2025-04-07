92c951bab98540084deb524825e3ec4d824f6032
Matheus Oliveira <59532044+radonix@users.noreply.github.com>
2025-04-07 16:10:27 -0300

    melhorei mais

--- src/components/MinhaConta/MinhaConta.tsx
+++ src/components/MinhaConta/MinhaConta.tsx
@@ -1,6 +1,6 @@
 "use client";
 import foto_perfil from "@/app/WIN_20200420_15_20_41_Pro.jpg";
-import edit_pencil from "@/public/MinhaConta/edit.png";
+import edit_pencil from "../../../public/MinhaConta/edit.svg"
 interface Usuario {
     nome: string,
     email: string,
@@ -19,46 +19,79 @@ export default function MinhaConta(props: Usuario) {
     return (
         <div className="min-h-screen flex items-center justify-center">
             <div className="minha-conta text-center">
-            <h1>Minha Conta</h1>
-            <img
-                src={usuario.foto_perfil}
-                alt="Foto de perfil"
-                className="w-50 h-50 rounded-full object-cover mx-auto"
-            />
-            <form className="mt-4">
-                <label className="flex items-center gap-2 justify-center">
-                Nome:
-                <input
-                    type="text"
-                    defaultValue={usuario.nome}
-                    placeholder="Digite seu nome"
-                    className="border rounded px-2 py-1"
+                <h1>Minha Conta</h1>
+                <img
+                    src={usuario.foto_perfil}
+                    alt="Foto de perfil"
+                    className="w-50 h-50 rounded-full object-cover mx-auto"
                 />
-                <span className="material-icons text-black cursor-pointer"></span>
-                </label>
-                <br />
-                <label className="flex items-center gap-2 justify-center">
-                Email:
-                <input
-                    type="email"
-                    defaultValue={usuario.email}
-                    placeholder="Digite seu email"
-                    className="border rounded px-2 py-1"
-                />
-                <span className="material-icons text-black cursor-pointer"></span>
-                </label>
-                <br />
-                <label className="flex items-center gap-2 justify-center">
-                Senha:
-                <input
-                    type="password"
-                    defaultValue={usuario.senha}
-                    placeholder="Digite sua senha"
-                    className="border rounded px-2 py-1"
-                />
-                <span className="material-icons text-black cursor-pointer"></span>
-                </label>
-            </form>
+                <form className="mt-4">
+                    <label className="flex items-center gap-2 justify-center">
+                        Nome:
+                        <input
+                            type="text"
+                            defaultValue={usuario.nome}
+                            placeholder="Digite seu nome"
+                            className="border rounded px-2 py-1"
+                            readOnly
+                        />
+                        <button
+                            type="button"
+                            className="material-icons text-black cursor-pointer"
+                            onClick={(e) => {
+                                const input = (e.currentTarget.previousSibling as HTMLInputElement);
+                                input.readOnly = !input.readOnly;
+                                if (!input.readOnly) input.focus();
+                            }}
+                        >
+                            <img src={edit_pencil.src} alt="Edit" />
+                        </button>
+                    </label>
+                    <br />
+                    <label className="flex items-center gap-2 justify-center">
+                        Email:
+                        <input
+                            type="email"
+                            defaultValue={usuario.email}
+                            placeholder="Digite seu email"
+                            className="border rounded px-2 py-1"
+                            readOnly
+                        />
+                        <button
+                            type="button"
+                            className="material-icons text-black cursor-pointer"
+                            onClick={(e) => {
+                                const input = (e.currentTarget.previousSibling as HTMLInputElement);
+                                input.readOnly = !input.readOnly;
+                                if (!input.readOnly) input.focus();
+                            }}
+                        >
+                           <img src={edit_pencil.src} alt="Edit" />
+                        </button>
+                    </label>
+                    <br />
+                    <label className="flex items-center gap-2 justify-center">
+                        Senha:
+                        <input
+                            type="password"
+                            defaultValue={usuario.senha}
+                            placeholder="Digite sua senha"
+                            className="border rounded px-2 py-1"
+                            readOnly
+                        />
+                        <button
+                            type="button"
+                            className="material-icons text-black cursor-pointer"
+                            onClick={(e) => {
+                                const input = (e.currentTarget.previousSibling as HTMLInputElement);
+                                input.readOnly = !input.readOnly;
+                                if (!input.readOnly) input.focus();
+                            }}
+                        >
+                            <img src={edit_pencil.src} alt="Edit" />
+                        </button>
+                    </label>
+                </form>
             </div>
         </div>
     );

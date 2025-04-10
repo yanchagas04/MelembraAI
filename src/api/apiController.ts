eb301b3f2fce4566e62af6f6aaad055b36679285
yanchagasPC <yanchagas2004@gmail.com>
2025-04-10 09:17:29 -0300

    feat: Trocamos a API para sugestão de atividades e tradução das mesmas. Email não estava funcionando...

--- src/api/apiController.ts
+++ src/api/apiController.ts
@@ -1,50 +1,10 @@
-"use client"
-require('dotenv').config()
-export const SENDER = process.env.SENDER
-const KEY = process.env.API_KEY as string
-const SECRET_KEY = process.env.API_SECRET_KEY as string
-// const mailjet = require('node-mailjet').connect(
-//     KEY,
-//     SECRET_KEY
-//   )
-  
-import mailjet from 'node-mailjet'
-mailjet.apiConnect(KEY, SECRET_KEY)
+const ACTIVITY_API_URL = "https://apis.scrimba.com/bored/api/activity";
 
-export default async function enviarEmail(destinario: string) { 
-    let response = {
-      status: 200,
-      message: 'Email enviado com sucesso',
-    }
-    const request = mailjet.HttpMethods.Post) ('send', { version: 'v3.1' }).request({
-      Messages: [
-        {
-          From: {
-            Email: SENDER,
-            Name: 'MelembraAI',
-          },
-          To: [
-            {
-              Email: destinario,
-              Name: 'Usuário',
-            },
-          ],
-          Subject: 'Email da plataforma de tarefas!',
-          TextPart: 'Olá!',
-          HTMLPart:
-            '<h3>Dear passenger 1, welcome to <a href="https://www.mailjet.com/">Mailjet</a>!</h3><br />May the delivery force be with you!',
-        },
-      ],
-    })
-    request
-      .then((result: { body: any }) => {
-        console.log(result.body)
-        return response
-      })
-      .catch((err: { statusCode: any }) => {
-        console.log(err.statusCode)
-        response.status = 500
-        response.message = 'Erro ao enviar o email'
-        return response
-      })
+export async function gerarAtividade() {
+    const response = await fetch(ACTIVITY_API_URL);
+    const data = await response.json();
+    const atividade = data.activity;
+    const response2 = await fetch(`https://api.mymemory.translated.net/get?q=${encodeURIComponent(atividade)}&langpair=en|pt`);
+    const data2 = await response2.json();
+    return data2.responseData.translatedText;
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,20 @@
 "use client";
-import enviarEmail from "@/api/apiController";
+
+import { gerarAtividade } from "@/api/apiController";
+import { useState } from "react";
 
 export default function Home() {
+  const [atividade, setAtividade] = useState<string | null>(null);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>MelembraAI</h1>
-      <button onClick={async () => {
-        await enviarEmail('yanchagas2004@gmail.com')
-      }}>Enviar</button>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={
+        async () => {
+          const atividade = await gerarAtividade();
+          setAtividade(atividade);
+        }
+      }>Enviar</button>
+      {atividade && <p>{atividade}</p>}
     </main>
   );
 }
